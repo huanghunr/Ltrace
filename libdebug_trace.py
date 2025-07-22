54b70a5db258585e1fb4083579460874fc2c9025
huanghunr <huanghunr@outlook.com>
2025-07-22 19:24:26 +0800

    修复了程序运行获取不到库的问题

--- libdebug_trace.py
+++ libdebug_trace.py
@@ -5,6 +5,7 @@ import os
 import signal
 import threading
 import time
+import io
 from capstone import *
 from capstone.x86 import *
 from tqdm import tqdm
@@ -17,7 +18,7 @@ context.log_level ="error"
 # libcontext.sym_lvl = 4
 TIMEOUT = 5
 
-def exit_program(d):
+def exit_program(d:object):
     """
     当计时器超时时执行的函数，用于退出程序。
     """
@@ -42,14 +43,15 @@ x64_regs = [
     "ax", "bx", "cx", "dx", "si", "di", "bp", "sp",
     "r8w", "r9w", "r10w", "r11w", "r12w", "r13w", "r14w", "r15w",
 
-
     "al", "bl", "cl", "dl", "sil", "dil", "bpl", "spl",
     "r8b", "r9b", "r10b", "r11b", "r12b", "r13b", "r14b", "r15b",
     "ah", "bh", "ch", "dh"
 ]
 
-
-def dump_registers_to_file(d, f):
+def dump_registers_to_file(d:object, f:io.TextIOBase):
+    """
+    打印寄存器的初始值
+    """
     regs = d.regs
     general_regs = [
         'rax', 'rbx', 'rcx', 'rdx', 'rdi', 'rsi',
@@ -83,15 +85,49 @@ def dump_registers_to_file(d, f):
     f.write("\n-----------\n")
 
 def get_binary_name(filepath):
+    """
+    获取程序的名称
+    """
     path = filepath.split("/")
     return path[-1]
 
-def get_libs_info(d, binary_file_name):
+def get_libs_info(d,binary_file_name):
     """
     获取程序以及标准库的数据，包括名称、路径、起止地址、导出符号
     """
     try:
         libs_info = {}
+        maps = d.maps
+        for m in maps:
+            path = m.backing_file
+            if not path or not path.startswith("/"):
+                continue
+            if "ld-linux-x86-64.so.2" in path or binary_file_name in path:
+                continue
+            if path not in libs_info:
+                lib_name = path.split("/")[-1]
+                libs_info[path] = {"name": lib_name, "base": m.start, "end": m.end, "symbol":{}}
+            else:
+                libs_info[path]["base"] = min(libs_info[path]["base"], m.start)
+                libs_info[path]["end"] = max(libs_info[path]["end"], m.end)
+        
+        for i in libs_info:
+            lib = ELF(i)
+            libs_info[i]["symbol"] = {libs_info[i]["base"] + addr: name for name, addr in lib.symbols.items()}
+
+        return libs_info
+    except Exception as e:
+        d.print_maps()
+        d.kill()
+        print(e)
+        print("error: 无法读取标准库，请在结束所有与追踪程序相关进程后再继续")
+        exit(0)
+
+def get_binary_info(d, binary_file_name):
+    """
+    获取程序以及标准库的数据，包括名称、路径、起止地址、导出符号
+    """
+    try:
         binary_info ={}
         maps = d.maps
         for m in maps:
@@ -105,25 +141,12 @@ def get_libs_info(d, binary_file_name):
                     binary_info["base"] = min(binary_info["base"], m.start)
                     binary_info["end"] = max(binary_info["end"], m.end)
                 continue
-            elif "ld-linux-x86-64.so.2" in path:
-                continue
-            if path not in libs_info:
-                lib_name = path.split("/")[-1]
-                libs_info[path] = {"name": lib_name, "base": m.start, "end": m.end, "symbol":{}}
-            else:
-                libs_info[path]["base"] = min(libs_info[path]["base"], m.start)
-                libs_info[path]["end"] = max(libs_info[path]["end"], m.end)
-        
-        for i in libs_info:
-            lib = ELF(i)
-            libs_info[i]["symbol"] = {libs_info[i]["base"] + addr: name for name, addr in lib.symbols.items()}
-
-        return binary_info,libs_info
+        return binary_info
     except Exception as e:
         d.print_maps()
         d.kill()
         print(e)
-        print("error: 无法读取标准库，请在结束所有与追踪程序相关进程后再继续")
+        print("error: 无法读取程序信息，请在结束所有与追踪程序相关进程后再继续")
         exit(0)
 
 def get_entrypoint(filepath):
@@ -151,16 +174,28 @@ def handle_lib_func(d,ripaddr, f, lib_info, binary_info):
                 func_name = lib_info[lib_path]["symbol"][ripaddr]
             else:
                 func_name = f"unknown_lib_function({lib_info[lib_path]["name"]})"
-            f.write(f"{hex(ripaddr)}\tlibc+{hex(ripaddr-libc_base)}\t{func_name}\n")
+            f.write(f"{hex(ripaddr)}\t{lib_info[lib_path]["name"]}+{hex(ripaddr-libc_base)}\t{func_name}\n")
             # print(func_name,ret_addr)
-            while(1):
-                d.step()
-                rip = d.regs.rip
-                if rip >= binary_info["base"] and rip <= binary_info["end"]:
-                    return 1
+            try:
+                while(1):
+                    d.step()
+                    rip = d.regs.rip
+                    # f.write(f"{hex(rip)}\n")
+                    if rip >= binary_info["base"] and rip <= binary_info["end"]:
+                        return 1
+                    # handle_lib_func(d,rip, f, lib_info, binary_info)
+                    
+            except RuntimeError as e:
+                if str(e) =="All threads are dead." or str(e) == "The thread is dead.":
+                    print(f"追踪程序已运行结束。退出代码：{d.exit_code}; 退出信号：{d.exit_signal}")
+                    f.close()
+                    exit(0)
+                else:
+                    print("运行错误",e)
+                    exit(1)
     return 0
 
-def trace_file(filepath:str, args: str, startaddr:int, maxlen:int, output:str, env:dict, inputdata:list):
+def trace_file(filepath:str, args: list, startaddr:int, maxlen:int, output:str, env:dict, inputdata:list):
     """
     通过二进制文件启动追踪
     """
@@ -179,6 +214,8 @@ def trace_file(filepath:str, args: str, startaddr:int, maxlen:int, output:str, e
                      auto_interrupt_on_command=True, continue_to_binary_entrypoint=False,escape_antidebug=True,env=env)
     p = d.run()
     pid = d.pid
+    
+    d.pprint_maps()
 
     global count
     count = 0
@@ -192,16 +229,17 @@ def trace_file(filepath:str, args: str, startaddr:int, maxlen:int, output:str, e
     if len(inputdata) > 0: d.handle_syscall("read",on_enter=handle_read(inputdata))
 
     rip  = lambda: d.regs.rip
+    
+    # 获取二进制文件的信息，包括起止地址
+    binary_info = get_binary_info(d,binary_file_name)
 
-    #获取程序以及标准库的数据，包括名称、路径、起止地址、导出符号
-    binary_info, lib_info = get_libs_info(d,binary_file_name)
-
-    #计数器用于检查初始位置是否合法
+    # 计数器用于检查初始位置是否合法
     timer = threading.Timer(TIMEOUT, exit_program,args=(d))
     timer.daemon = True
     timer.start()
+    f = open(output, "w")
 
-    #运行到指定的开始地址，没有指定默认从入口点开始
+    # 运行到指定的开始地址，没有指定默认从入口点开始
     base_addr = binary_info["base"]
     if startaddr != 0:
         bp1 = d.breakpoint(startaddr, hardware=True, file = binary_file_name)
@@ -223,23 +261,31 @@ def trace_file(filepath:str, args: str, startaddr:int, maxlen:int, output:str, e
                 # print(hex(base_addr+startaddr),hex(rip()))
                 break
 
-    #如果初始位置正确，取消计时器
+    # 如果初始位置正确，取消计时器
     timer.cancel()
 
-    f = open(output, "w")
-
+    # 获取程序以及标准库的数据，包括名称、路径、起止地址、导出符号(不能在太前面运行，用于确保所有库已经加载进去)
+    lib_info = get_libs_info(d,binary_file_name)
     dump_registers_to_file(d, f)
-    ret_addr = None
     
     for i in tqdm(range(maxlen)):
         ripaddr = rip()
-        # print(hex(ripaddr))
-        CODE = d.memory[ripaddr,16]
+
+        try:
+            CODE = d.memory[ripaddr,16]
+        except RuntimeError as e:
+            if str(e) =="All threads are dead.":
+                print(f"追踪程序已运行结束。退出代码：{d.exit_code}; 退出信号：{d.exit_signal}")
+                f.close()
+                exit(0)
+            else:
+                print("解析错误",e)
+                exit(1)
         address, size, mnemonic, op_str = next(md.disasm_lite(CODE, 0x1))
         registers = [reg.strip() for reg in op_str.split(',')]
         # print(mnemonic,op_str)
-
-        lib_func_handler=handle_lib_func(d,ripaddr,f,lib_info,ret_addr,binary_info)
+        # print(binary_info)
+        lib_func_handler=handle_lib_func(d,ripaddr,f,lib_info,binary_info)
         if lib_func_handler: continue
 
         # 右侧打印寄存器和内存
@@ -249,8 +295,6 @@ def trace_file(filepath:str, args: str, startaddr:int, maxlen:int, output:str, e
         if reg_name in x64_regs:
             d.step()
             reg_str = ''.join(f"{reg_name:<3}={getattr(d.regs,reg_name):#018x}")
-            rsp = d.regs.rsp
-            ret_addr = int.from_bytes(d.memory[rsp : rsp + 8], "little")
 
         # 遇到call时打印参数寄存器
         elif mnemonic == "call":
@@ -268,7 +312,7 @@ def trace_file(filepath:str, args: str, startaddr:int, maxlen:int, output:str, e
                 seg = insn.reg_name(op.mem.segment)
                 scale = op.mem.scale
                 disp = op.mem.disp
-                if not seg is None:
+                if not seg is None or mnemonic == "nop":
                     d.step()
                 elif index is None or index == "riz":
                     mem_addr = getattr(d.regs, base) + disp
@@ -276,7 +320,7 @@ def trace_file(filepath:str, args: str, startaddr:int, maxlen:int, output:str, e
                     mem_addr = getattr(d.regs, base) + getattr(d.regs, index) * scale + disp
                 try:
                     d.step()
-                    # if mem_addr > 0xffffffffffff:print(hex(mem_addr))
+                    # print(hex(mem_addr),mnemonic,op_str,ripaddr)
                     mem_data_qword = int.from_bytes(d.memory[(mem_addr&0xffffffffffffffff),8], "little")
                     reg_str = f"mem={mem_data_qword:#018x}"
                 except Exception as e:
